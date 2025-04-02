d3b28b3421533a734ca8f78c21f08d917ec06d47
linshuhao.shihao <linshuhao.shihao@bytedance.com>
2025-04-02 21:44:43 +0800

    todo-list

--- src/App.tsx
+++ src/App.tsx
@@ -6,40 +6,120 @@ import lynxLogo from './assets/lynx-logo.png'
 import reactLynxLogo from './assets/react-logo.png'
 
 export function App() {
-  const [alterLogo, setAlterLogo] = useState(false)
+  const [todos, setTodos] = useState<string[]>(['0', '1'])
+  const [itemCount, setItemCount] = useState<number>(2)
+
 
   useEffect(() => {
     console.info('Hello, ReactLynx')
   }, [])
 
-  const onTap = useCallback(() => {
-    'background only'
-    setAlterLogo(!alterLogo)
-  }, [alterLogo])
+  const addTodo = useCallback(() => {
+    setItemCount(itemCount + 1)
+    setTodos([...todos, itemCount.toString()])
+  }, [todos])
+
+  const deleteTodoCurrentItem = useCallback((index: number) => {
+    const newTodos = todos.filter((_, i) => i !== index)
+    setTodos(newTodos)
+  }, [todos])
 
   return (
-    <view>
-      <view className='Background' />
-      <view className='App'>
-        <view className='Banner'>
-          <view className='Logo' bindtap={onTap}>
-            {alterLogo
-              ? <image src={reactLynxLogo} className='Logo--react' />
-              : <image src={lynxLogo} className='Logo--lynx' />}
-          </view>
-          <text className='Title'>React</text>
-          <text className='Subtitle'>on Lynx</text>
+    <view style={{
+      backgroundColor: "white",
+      padding: "20px",
+      width: "100%",
+      height: "100vh",
+      border: "2px solid black",
+      borderRadius: "10px"
+    }}>
+      <view style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginBottom: "20px"
+      }}>
+        <view
+          style={{
+            backgroundColor: "#f0f5ff",
+            border: "2px solid black",
+            borderRadius: "10px",
+            padding: "15px",
+            width: "100%",
+            alignItems: "center",
+            justifyContent: "center"
+          }}
+          bindtap={addTodo}
+        >
+          <text style={{ textAlign: "center", fontSize: "18px", color: "black" }}>Add</text>
         </view>
-        <view className='Content'>
-          <image src={arrow} className='Arrow' />
-          <text className='Description'>Tap the logo and have fun!</text>
-          <text className='Hint'>
-            Edit<text style={{ fontStyle: 'italic' }}>{' src/App.tsx '}</text>
-            to see updates!
-          </text>
-        </view>
-        <view style={{ flex: 1 }}></view>
       </view>
+
+      <view style={{
+        backgroundColor: "#f0f5ff",
+        border: "2px solid black",
+        borderRadius: "10px",
+        padding: "15px",
+        marginBottom: "10px",
+        alignItems: "center",
+        justifyContent: "center"
+      }}>
+        <text style={{ textAlign: "center", fontSize: "18px", color: "black" }}>todo-list</text>
+      </view>
+      <list
+        scroll-orientation="vertical"
+        list-type="single"
+        span-count={1}
+        style={{
+          width: "100%",
+          height: "100vh",
+          listMainAxisGap: "0px",
+        }}
+      >
+        {todos.map((item, index) => {
+          return (
+            <list-item
+              item-key={`list-item-${index}`}
+              key={`list-item-${index}`}
+              style={{
+                marginBottom: "10px",
+              }}
+            >
+              <view style={{
+                display: "flex",
+                flexDirection: "row",
+                width: "100%",
+              }}>
+                <view style={{ 
+                  width: "50%", 
+                  backgroundColor: "#f0f5ff",
+                  border: "2px solid black",
+                  borderRadius: "10px",
+                  padding: "15px",
+                  alignItems: "center", 
+                  justifyContent: "center" 
+                }}>
+                  <text style={{ textAlign: "center", fontSize: "18px", color: "black" }}>item {item}</text>
+                </view>
+                <view style={{ 
+                  width: "50%", 
+                  backgroundColor: "#f0f5ff",
+                  border: "2px solid black",
+                  borderRadius: "10px",
+                  padding: "15px",
+                  alignItems: "center", 
+                  justifyContent: "center" 
+                }}>
+                  <text
+                    style={{ textAlign: "center", fontSize: "18px", color: "black" }}
+                    bindtap={() => deleteTodoCurrentItem(index)}
+                  >deleteItem</text>
+                </view>
+              </view>
+            </list-item>
+          );
+        })}
+      </list>
     </view>
   )
 }
